d1cb36d50a3d82a0d0647b82bf235c10eca3e184
taranjeet singh <taranjeet.singh@evontech.com>
2020-04-23 13:13:22 +0530

    Integrated nested attributes for portfolio models

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,9 @@ class Portfolio < ApplicationRecord
 	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 	has_many :technologies
+	# accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
+	accepts_nested_attributes_for :technologies,
+																reject_if: lambda { |attrs| attrs['name'].blank? }
 
 	def self.angular
 		where(subtitle: 'Angular')
