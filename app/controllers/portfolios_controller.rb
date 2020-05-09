ad8a3ef216cce5e31eacb04b952040c0d8aeacf9
taranjeet singh <taranjeet.singh@evontech.com>
2020-05-09 23:51:32 +0530

    Integrated cocoon for portfolio items nested form component

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,7 +21,7 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
+    # 3.times { @portfolio_item.technologies.build }
 	end
 
   def create
@@ -68,12 +68,13 @@ class PortfoliosController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def portfolio_params
+    # technologies_attributes: [:id, :name, :_destroy] we passed _destroy here because it is needed by cocoon gem for deleting the associated attrbute
     params.require(:portfolio).permit(:title, 
                                       :subtitle, 
                                       :body,
                                       :main_image,
                                       :thumb_image,
-                                      technologies_attributes: [:name]
+                                      technologies_attributes: [:id, :name, :_destroy]
                                       )
   end
 
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,6 +4,7 @@ class Portfolio < ApplicationRecord
 	has_many :technologies
 	# accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
 	accepts_nested_attributes_for :technologies,
+																allow_destroy: true,
 																reject_if: lambda { |attrs| attrs['name'].blank? }
 
 	mount_uploader :thumb_image, PortfolioUploader
