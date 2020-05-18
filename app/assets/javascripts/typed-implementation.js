b41a23d30d80a847b03167a093fd84725da8db2c
taranjeet singh <taranjeet.singh@evontech.com>
2020-05-18 23:11:27 +0530

    Implemented the typed.js library

---
+++ app/assets/javascripts/typed-implementation.js
@@ -0,0 +1,9 @@
+$( document ).on('turbolinks:load', function() {
+	  var typed = new Typed('#element', {
+		  strings: [
+				'Software development is the closest concept we have to magic.',
+				"It's my goal to ensure that anyone with a passion for coding can become a true programming artisan."
+			],
+			typeSpeed: 0
+		});
+});
